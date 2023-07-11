ae15d7ff2b7c5b034f74a1e97e430c09a40e3528
jinnyl22 <smile2374@gmail.com>
2023-07-11 22:15:43 +0900

    study:230711 object, function

---
+++ JS/fn.js
@@ -0,0 +1,10 @@
+function f(a, b) {
+  console.log(a, b);
+}
+f(1, 2);
+f([1, 2]);
+
+function f1([a, b]) {
+  console.log(a, b);
+}
+f1([1, 2]);
---
+++ JS/obj_property.js
@@ -0,0 +1,29 @@
+/**
+ * 객체 - 프로퍼티 키(key) 표현 규칙
+ */
+const user = {
+  "": 1,
+  " ": 1, // 'id': 1, '0y': 2 모두 OK!
+  123: 1, // user[123], user['123'] OK, but user.123 is SyntaxError!!
+  12345n: 2, // user[12345], user[12345n], user['12345'] OK, but user['12345n'] is undefined!
+  true: 1, // OK  user[true]  user.true
+  id: 2,
+  [`name`]: "Hong", // But, `name`: 'Hong'은 SyntaxError: Unexpected template string!
+  [Symbol()]: "Hong", // OK But, Symbol(): 'Hong'은 SyntaxError: Unexpected token ':'
+  [`${new Date()}`]: 365, // OK! 'Sun Jul …': 365
+  "my-friends": ["Han", "Kim"],
+  getInfo: () => `${this.id}-${this.name}`, // OK! But, this is not user!
+  getInfo() {
+    return `${this.id}-${this.name}`;
+  }, // OK! getInfo의 최종 <f.o>
+};
+
+console.table(user);
+console.log("------------------");
+console.log(user);
+console.log("obj keys > " + Object.keys(user));
+
+for (const [k, v] of Object.entries(user)) {
+}
+for (const k of Object.keys(user)) {
+}
