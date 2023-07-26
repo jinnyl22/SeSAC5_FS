38f6bfab808743f05e5f57a5f8a3feb76a1c39af
jinnyl22 <smile2374@gmail.com>
2023-07-26 12:31:52 +0900

    TryThis:prototype mapBy

--- JS/Try This/prototype_array.js
+++ JS/Try This/prototype_array.js
@@ -8,17 +8,28 @@ const lee = { id: 3, name: "Lee" };
 
 const arr = [1, 2, 3, 4, 5];
 
-Array.prototype.mapBy = function () {
-  return this.id.map((el) => {});
+Array.prototype.mapBy = function (args) {
+  const mapArr = this.map((el) => el[args]);
+  console.log(mapArr);
 };
 
-Object.defineProperties();
+// Array.prototype.mapBy = function (args) {
+//   const mapArr = [];
+//   //   console.log(args);
+//   for (let i = 0; i < this.length; i++) {
+//     mapArr[i] = this[i][args];
+//   }
+//   console.log(mapArr);
+//   return mapArr;
+// };
 
-arr.firstObject; // 1
-arr.lastObject; // 5
+// Object.defineProperties();
+
+// arr.firstObject; // 1
+// arr.lastObject; // 5
 
 const users = [hong, kim, lee]; // {id:1, name: 'Hong'}, …
 users.mapBy("id"); // [1, 2, 3]
 users.mapBy("name"); // ['Hong', 'Kim', 'Lee']
-users.filterBy(id, 2); // [{id: 2, name: 'Kim'}]
-users.findBy("name", "Kim"); // {id: 2, name: 'Kim'}
+// users.filterBy(id, 2); // [{id: 2, name: 'Kim'}]
+// users.findBy("name", "Kim"); // {id: 2, name: 'Kim'}
