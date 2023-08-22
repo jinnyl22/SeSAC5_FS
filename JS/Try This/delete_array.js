f9c010a25d25ad3a72f42d420132a0cea2535a9e
jinnyl22 <smile2374@gmail.com>
2023-08-23 02:14:43 +0900

    TT:230822 deleteArray fin, deleteObjectArray ing

--- JS/Try This/delete_array.js
+++ JS/Try This/delete_array.js
@@ -3,21 +3,23 @@
  */
 
 const arr = [1, 2, 3, 4];
-const user = [
+const users = [
   { id: 1, name: "Hong" },
   { id: 2, name: "Kim" },
   { id: 3, name: "Lee" },
 ];
 
-const deleteArray = (arr, start, end = arr.length - 1) => {
-  let arr1 = [...arr];
-  arr1.splice(start, end - 1);
-  return arr1;
+const deleteArray = (arr, start, end = arr.length) => {
+  const start1 = arr.slice(0, start);
+  const end1 = arr.slice(end);
+
+  return [...start1, ...end1];
 };
 
-// const deleteObjectArray = (arr, ...args)=>{
-//     const {}
-// }
+const deleteObjectArray = (arr, ...args) => {
+  const [k, v] = [...args];
+  return arr.filter((el) => el[k] !== v);
+};
 
 // 2번 인덱스부터 삭제
 console.log(deleteArray(arr, 2)); // [1, 2]
@@ -25,7 +27,10 @@ console.log(deleteArray(arr, 3)); // [1, 2, 3]
 // 1번 인덱스부터 3-1번까지(1,2번 인덱스) 삭제
 console.log(deleteArray(arr, 1, 3)); // [1, 4]
 console.log(deleteArray(arr, 1, 4)); // [1]
-// deleteObjectArray(users, 2); // Hong, Kim
-// deleteObjectArray(users, "id", 2); // Hong, Lee
-// deleteObjectArray(users, "name", "Lee"); // Hong, Kim
-console.log("원본배열 >>>", arr);
+console.log(deleteArray(arr, 0, 3)); // [4]
+
+// 2번 인덱스 삭제
+// console.log(deleteObjectArray(users, 2)); // Hong, Kim
+console.log(deleteObjectArray(users, "id", 2)); // Hong, Lee
+console.log(deleteObjectArray(users, "name", "Lee")); // Hong, Kim
+console.log("원본배열 >>", arr);
