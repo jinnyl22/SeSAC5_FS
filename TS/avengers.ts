1835a68ef082fece2022941b70a9554e7501b42d
jinnyl22 <smile2374@gmail.com>
2023-08-26 23:04:57 +0900

    study:230826 nest02, decorator

---
+++ TS/avengers.ts
@@ -0,0 +1,56 @@
+/**
+ * 두개의 클래스를 상속을 받지 않고 합치는 것!
+ */
+
+interface Avengers {
+  moveout(): void;
+}
+
+// 정석대로 가면 Avenger라는 사람에 무기를 붙인다는 느낌
+// 아래에서 peter를 생성자로 받음! peter를 발전시키는 것임!
+class Avenger implements Avengers {
+  constructor(public name: string) {}
+  moveout() {}
+}
+
+// 데코레이터!
+// 공통기능을 여기다가 모아두면 되는 것임!
+class BaseAvengers {
+  constructor(public avenger: Avengers) {}
+  moveout() {
+    this.avenger.moveout();
+  }
+}
+
+// implements하면 스파이더맨은 어벤져스 타입이 되는 것임!
+class SpiderMan extends BaseAvengers {
+  moveout() {
+    super.moveout();
+    console.log("Spider Web!!");
+  }
+}
+
+// 데코레이터 사용 전
+// class IronMan implements Avengers {
+//   moveout() {
+//     console.log("ParmFire!");
+//   }
+// }
+class IronMan extends BaseAvengers {
+  moveout() {
+    super.moveout();
+    console.log("ParmFire!");
+  }
+}
+
+const hero = new Avenger("Peter");
+// const hero1 = new SpiderMan(hero);
+// hero1.moveout();
+
+// const hero2 = new IronMan(hero);
+// hero2.moveout();
+
+// 클래스 데코레이터 패턴!
+const newHero = new IronMan(new SpiderMan(hero));
+newHero.moveout();
+console.log("--------------------------------");
---
+++ TS/dogcat.ts
@@ -0,0 +1,80 @@
+/**
+ * 인터페이스를 쓰지 않고 메서드로 합침
+ */
+
+class Animal {
+  #name;
+  constructor(name: string) {
+    this.#name = name;
+  }
+  bark() {}
+  getName() {
+    return this.#name;
+  }
+}
+
+class BaseAnimal extends Animal {
+  // 오버라이딩
+  private ani: Animal;
+  constructor(aniOrName: string | Animal) {
+    super(typeof aniOrName === "string" ? aniOrName : aniOrName.getName());
+    this.ani = typeof aniOrName === "string" ? new Animal(aniOrName) : aniOrName;
+  }
+  bark(): void {
+    super.bark();
+    this.ani.bark();
+  }
+}
+
+class Dog extends BaseAnimal {
+  bark() {
+    super.bark();
+    console.log("멍멍");
+  }
+}
+class Cat extends BaseAnimal {
+  bark() {
+    super.bark();
+    console.log("야옹");
+  }
+}
+
+const x = new Dog(new Cat("개냥이"));
+x.bark();
+
+class X {
+  constructor(public id: number) {}
+  f() {
+    console.log("f >>", this.id);
+  }
+}
+
+const xx = new X(123);
+
+console.log("----------------------------------------");
+
+//
+const fns = {
+  id: xx.id,
+  //   f: xx.f(),
+  // 이렇게 가져온 건 f라는 <f.o>를 그냥 가져온 것!
+  // function pool에 있는 <f.o>를 참조하고 있기 때문에
+  // fns의 env와 x의 env가 같은 <f.o>를 바라보고 있지만 호출되었을 때의 this는 다름!
+  f: xx.f,
+  barkDog() {
+    console.log("멍!멍!멍!");
+    return this;
+  },
+  barkCat() {
+    console.log("야옹야옹~");
+    return this;
+  },
+};
+
+// 이런 패턴이 디자인패턴에서 빌더 패턴임!
+// fns.barkCat().barkDog();
+// apply를 안주면 f는 undefined가 된다! this로 xx를 넣어준것
+// fns.barkCat().barkDog().f.apply(xx);
+fns.barkCat().barkDog().f();
+// fns.barkCat();
+// fns.barkDog();
