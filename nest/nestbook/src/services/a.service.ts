9861846ac382fa3dbd5b8bfd9641d4aa3e47dd9d
jinnyl22 <smile2374@gmail.com>
2023-09-01 03:14:24 +0900

    TIL:230901 provider injectable

--- nest/nestbook/src/services/a.service.ts
+++ nest/nestbook/src/services/a.service.ts
@@ -1,6 +1,8 @@
 // import { Injectable } from '@nestjs/common';
 
 // @Injectable()
+// 이 클래스는 프로바이더!
+// @Injectable()을 지워도 가능한 건 주입을 받는 것이 없기때문!
 export class ServiceA {
   getHello(): string {
     return 'Hello World A!';
--- nest/nestbook/src/services/b.service.ts
+++ nest/nestbook/src/services/b.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+// import { Injectable } from '@nestjs/common';
 import { BaseService } from './base-service';
+// import { ServiceA } from './a.service';
 
-@Injectable()
+// @Injectable()
 export class ServiceB extends BaseService {
+  // constructor(private readonly _serviceA: ServiceA) {
+  //   super(_serviceA);
+  // }
   getHello(): string {
     return this.doSomeFuncFromA();
   }
